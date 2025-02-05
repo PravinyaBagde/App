81b008fba57de83d296d7c9706c90e1b6964a436
Pravinya1 <bagdepravinya@gmail.com>
2025-02-05 10:21:54 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+function greetUser() {
+    let currentHour = new Date().getHours();
+    let greeting;
+
+    if (currentHour < 12) {
+        greeting = "Good Morning!";
+    } else if (currentHour < 18) {
+        greeting = "Good Afternoon!";
+    } else {
+        greeting = "Good Evening!";
+    }
+
+    console.log(greeting);
+}
+
+greetUser();
